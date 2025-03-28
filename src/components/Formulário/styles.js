967829d65a2602b7858bb6e6f3051d1fee627427
Daniel Costa <daniel.dc697@gmail.com>
2025-03-28 12:55:41 -0300

    estilização do componente formulário

--- src/components/Formulário/styles.js
+++ src/components/Formulário/styles.js
@@ -3,13 +3,30 @@ import styled from "styled-components";
 const FormularioContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  margin-top: 20px;
+  margin-top: 1.5rem;
+  padding: 5rem 10rem;
+  max-width: 60rem;
+  margin: 0 auto;
+
+  margin-top: 5rem;
+
+  background: #f7f7f7;
+  box-shadow: 7px 7px 15px rgba(0, 0, 0, 0.08);
+  border-radius: 20px;
+
+  @media screen and (max-width: 768px) {
+    padding: 2rem;
+    max-width: 30rem;
+  }
 `;
 
 const FormularioTitle = styled.h1`
-  font-size: 24px;
-  color: #333;
+  font-size: 2rem;
+  text-align: start;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
 `;
 
 const FormularioForm = styled.form`
@@ -17,15 +34,77 @@ const FormularioForm = styled.form`
   flex-direction: column;
 `;
 
-const FormularioLabel = styled.label``;
+const FormularioLabel = styled.label`
+  font-size: 0.75rem;
+  margin-bottom: 0.25rem;
 
-const FormularioInput = styled.input``;
+  @media screen and (max-width: 768px) {
+    font-size: 0.6rem;
+  }
+`;
 
-const FormularioSelect = styled.select``;
+const FormularioInput = styled.input`
+  margin-bottom: 1rem;
+  background: #ffffff;
+  mix-blend-mode: normal;
+  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
+  padding: 1rem 1.5rem;
+  mix-blend-mode: normal;
+  &::placeholder {
+    color: #afafaf;
+    font-size: 0.75rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const FormularioSelect = styled.select`
+  margin-bottom: 1rem;
+  background: #ffffff;
+
+  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.06);
+  padding:1rem 1.5rem;
+
+  font-size: 0.75rem;
+
+  /* Estilo do select em si */
+  color: #afafaf; /* Cor do placeholder */
+
+  &:focus {
+    color: black; /* Cor do texto selecionado */
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 0.5rem 1rem;
+  }
+`;
 
 const FormularioOption = styled.option``;
 
-const FormularioButton = styled.button``;
+const FormularioButton = styled.button`
+  width: 20%;
+  background: #6278f7;
+  mix-blend-mode: normal;
+  border-radius: 10px;
+
+  padding: 1rem 1.5rem;
+  border: none;
+
+  font-family: 'Montserrat';
+  font-size:1rem;
+  
+  line-height: 24px;
+
+
+  color: #ffffff;
+
+  @media screen and (max-width: 768px) {
+    font-size: 0.6rem;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export {
   FormularioContainer,
@@ -37,4 +116,3 @@ export {
   FormularioOption,
   FormularioButton,
 };
-
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
+
 const GlobalStyle = createGlobalStyle`
   body {
+    font-family: "Prata", serif;
     box-sizing: border-box;
     margin: 0;
     padding: 0;
