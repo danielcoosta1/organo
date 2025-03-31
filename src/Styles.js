aa773552a6f278e9d78f4b7de3484899db04ed1b
Daniel Costa <daniel.dc697@gmail.com>
2025-03-31 09:28:34 -0300

    colocando display grid, para organizar os times por coluna

---
+++ src/Styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const TimeConteiner = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 24px;
+  padding: 24px;
+  max-width: 75rem;
+  margin: 0 auto;
+`;
+
+const Titulo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 3rem auto 3rem auto;
+  color: #6278f7;
+  font-family: "Prata", serif;
+
+  h1 {
+    border-bottom: 3px solid;
+  }
+`;
+
+export { TimeConteiner, Titulo };
