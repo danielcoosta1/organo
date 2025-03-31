94890f4c9920c0222929979f724cae3949b69e7d
Daniel Costa <daniel.dc697@gmail.com>
2025-03-31 09:27:50 -0300

    Att o componente Formulario. Limpando o código, reduzindo o uso de variaveis que estava usando com o useRef

--- src/components/Formulário/index.jsx
+++ src/components/Formulário/index.jsx
@@ -8,22 +8,31 @@ import {
 import InputDefault from "../InputDefault";
 import ListaSupensa from "../ListaSupensa";
 
-import { useRef } from "react";
+import { useState } from "react";
 
+const Formulario = ({ aoColaboradorCadastrado, times }) => {
+  const [formData, setFormData] = useState({
+    nome: "",
+    cargo: "",
+    imagem: "",
+    time: "",
+  });
 
-const Formulario = () => {
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((formData) => ({ ...formData, [id]: value }));
+  };
 
-  const inputName = useRef();
-  const inputCargo = useRef();
-  const inputImagem = useRef();
-
-  
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(inputName.current.value);
-    console.log(inputCargo.current.value);
-    console.log(inputImagem.current.value)
-
+    aoColaboradorCadastrado(formData);
+    setFormData({
+      // Reseta o formulário
+      nome: "",
+      cargo: "",
+      imagem: "",
+      time: "",
+    });
   };
 
   return (
@@ -32,43 +41,43 @@ const Formulario = () => {
         Preencha os dados para criar o card do colaborador
       </FormularioTitle>
       <FormularioForm onSubmit={handleSubmit}>
-      
         {/* Campo Nome */}
         <InputDefault
+          onChange={handleChange}
           label="Nome"
           id="nome"
           type="text"
           placeholder="Digite seu nome"
-          ref= {inputName}
         />
 
         {/* Campo Cargo */}
         <InputDefault
+          onChange={handleChange}
           label="Cargo"
           id="cargo"
           type="text"
           placeholder="Digite seu cargo"
-          ref={inputCargo}
-          
         />
 
         {/* Campo Imagem*/}
         <InputDefault
+          onChange={handleChange}
           label="Imagem"
           id="imagem"
           type="text"
           placeholder="Digite a URL da imagem"
-          ref={inputImagem}
         />
 
         {/* select */}
 
         <ListaSupensa
           label="Time"
-          id="time"
-          defaultValue=""
-        ></ListaSupensa>
-
+          id="time" // O mesmo nome do campo no estado
+          value={formData.time} // Valor controlado
+          onChange={handleChange} // Mesma função que os outros inputs
+          options={times}
+        />
+        <div></div>
         {/* button */}
 
         <FormularioButton type="submit">Criar card</FormularioButton>
