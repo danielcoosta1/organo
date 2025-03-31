b35d05336a84857b4d3051741fbdbd165a71f19d
Daniel Costa <daniel.dc697@gmail.com>
2025-03-31 15:30:03 -0300

    coloquei uma key válida no Card, essencial na sintaxe do react

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,14 +1,14 @@
 import Card from "../Card";
 import { DivCards, DivTime } from "./styles";
 
-const Time = ({ nome, corPrimaria, corSecundaria, colaboradores }) => {
+const Time = ({ nome, corPrimaria, corSecundaria, colaboradores,  }) => {
     return (
       <DivTime style={{ backgroundColor: corSecundaria }}>
         <h2 style={{ borderColor: corPrimaria }}>{nome}</h2>
         <DivCards>
           {colaboradores.map(colaborador => (
             <Card
-              key={colaborador.id}
+              key={colaborador.nome}
               nome={colaborador.nome}
               cargo={colaborador.cargo}
               imagem={colaborador.imagem}
