da2e0749b29ae6d1916f862590028a5c8a45180d
Daniel Costa <daniel.dc697@gmail.com>
2025-04-01 16:56:33 -0300

    att style do rodapé

--- src/components/Rodapé/styles.js
+++ src/components/Rodapé/styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const ContainerRodape = styled.footer`
   background-color: #6278f7; // Azul Alura
+  background-image: url("../../assets/fundo.png");
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -41,7 +42,7 @@ const Texto = styled.div`
   font-size: 1rem;
 
   @media (max-width: 768px) {
-    font-size: 1.25rem;
+    font-size: .75rem;
   }
 `;
 
